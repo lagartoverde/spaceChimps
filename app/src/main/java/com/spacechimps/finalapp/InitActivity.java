59814a0780c57ab493c95497859e8d8818e2d5e2
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2017-04-29 13:34:53 +0200

    commit

--- app/src/main/java/com/spacechimps/finalapp/InitActivity.java
+++ app/src/main/java/com/spacechimps/finalapp/InitActivity.java
@@ -2,12 +2,17 @@ package com.spacechimps.finalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MultiAutoCompleteTextView;
 
 public class InitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_screen);
+        setContentView(R.layout.loginscreen);
+    }
+
+    public void login(){
+        MultiAutoCompleteTextView user=findViewById();
     }
 }
---
+++ app/src/main/java/com/spacechimps/finalapp/Login.java
@@ -0,0 +1,9 @@
+package com.spacechimps.finalapp;
+
+/**
+ * Created by oscarrodriguez on 29/04/2017.
+ */
+
+public class Login extends appCompatActivity {
+
+}
