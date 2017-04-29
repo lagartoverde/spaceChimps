61af1b8a0246321da1a6613feb99261435010953
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2017-04-29 12:35:34 +0200

    initial Commit

---
+++ app/src/main/java/com/spacechimps/finalapp/InitActivity.java
@@ -0,0 +1,13 @@
+package com.spacechimps.finalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+}
