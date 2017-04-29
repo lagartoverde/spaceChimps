62a4ecc8ea4a0287e993f7a4223a34be29d19b6e
Kaysera <folez97@gmail.com>
2017-04-29 13:34:22 +0200

    User/Pass

--- app/src/main/java/com/spacechimps/finalapp/InitActivity.java
+++ app/src/main/java/com/spacechimps/finalapp/InitActivity.java
@@ -8,6 +8,6 @@ public class InitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_screen);
+        setContentView(R.layout.loginscreen);
     }
 }
