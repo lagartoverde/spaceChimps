a809b4e208b31af9e930f2e95761c6f4ff4e54db
Kaysera <folez97@gmail.com>
2017-04-29 17:44:12 +0200

    Commit

---
+++ app/src/main/java/com/spacechimps/finalapp/MainScreen.java
@@ -0,0 +1,15 @@
+package com.spacechimps.finalapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tomás on 29/04/2017.
+ */
+
+public class MainScreen extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainscreen);
+    }
+}
