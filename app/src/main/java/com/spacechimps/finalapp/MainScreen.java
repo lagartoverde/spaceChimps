95b5085bd802bd0ed850f93da75158a326af8241
Kaysera <folez97@gmail.com>
2017-04-29 19:22:05 +0200

    Main Screen

--- app/src/main/java/com/spacechimps/finalapp/MainScreen.java
+++ app/src/main/java/com/spacechimps/finalapp/MainScreen.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -27,17 +28,19 @@ import org.json.JSONObject;
 
 
 
-<<<<<<< HEAD
+
+
 public class MainScreen extends AppCompatActivity{
     RequestQueue requestQueue;
-=======
 
-public class MainScreen extends AppCompatActivity {
->>>>>>> origin/master
+    TextView welcome;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
-
+        welcome = (TextView) findViewById(R.id.welcome);
+        String userID = getIntent().getExtras().getString("username");
+        welcome.setText("Welcome, " + userID + ". Choose your desired playing game");
 
     }
 
