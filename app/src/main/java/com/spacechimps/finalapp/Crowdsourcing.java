60c782bb8fe7c37266a532009344bb0fdd896155
Kaysera <folez97@gmail.com>
2017-04-30 13:29:15 +0200

    Oscar y su dislexia

--- app/src/main/java/com/spacechimps/finalapp/Crowdsourcing.java
+++ app/src/main/java/com/spacechimps/finalapp/Crowdsourcing.java
@@ -1,6 +1,5 @@
 package com.spacechimps.finalapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,31 +12,30 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
  * Created by oscarrodriguez on 30/04/2017.
  */
 
-public class Crowsourcing extends AppCompatActivity {
+public class Crowdsourcing extends AppCompatActivity {
     static public TextView feedback;
     private RequestQueue requestQueue;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.crowsourcing);
+        setContentView(R.layout.crowdsourcing);
     }
     public void send(View v){
         TextView wordT=(TextView) findViewById(R.id.word);
         TextView definitionT=(TextView) findViewById(R.id.definition);
-        Crowsourcing.feedback=(TextView) findViewById(R.id.feedback);
+        Crowdsourcing.feedback=(TextView) findViewById(R.id.feedback);
         String word=wordT.getText().toString();
         String definition=definitionT.getText().toString();
         requestQueue = Volley.newRequestQueue(this.getBaseContext());
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, "http://spacechimps.ddns.net/controller.php?operation=7&word=" + word + "&definition=" + definition,
                 new Response.Listener<JSONObject>() {
                     public void onResponse(JSONObject response) {
-                           Crowsourcing.feedback.setText("Thanks for your input");
+                           Crowdsourcing.feedback.setText("Thanks for your input");
 
                     }
 
